bf607b3ff3663d4bd28d4fd4035fd8ed8b4baf31
Mayooran <mayooran@hsenidmobile.com>
2015-08-08 05:00:59 +0530

    Java parser module added

---
+++ modules/java-parser/src/main/java/org/madrona/java/parser/TestClazz.java
@@ -0,0 +1,16 @@
+package org.madrona.java.parser;
+
+public class TestClazz {
+
+    public TestClazz() {
+    }
+
+    public void method1() {
+        System.out.println("Hello");
+    }
+
+    private int method2(int param) {
+        System.out.println("World");
+        return param;
+    }
+}
